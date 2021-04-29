3d5fff5b92ef14e32de7a57fa95bac90be785b1c
cielpy <beijiu572@gmail.com>
2021-04-29 11:26:05 +0800

    refine code

--- build.sh
+++ build.sh
@@ -5,6 +5,5 @@
 ./scripts/feeds install -a
 
 make defconfig
-cp .config ./bin/
 
 make package/overture/{clean,compile} -j4 V=s
--- update-list.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-set -o pipefail
-
-wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/chnroute/chnroute.txt -nv -O files/overture/ip_network_primary_sample
-wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/chnroute/chnroute-v6.txt -nv -O files/overture/ip_network_primary_sample6
-cat files/overture/ip_network_primary_sample6 >> files/overture/ip_network_primary_sample
-rm files/overture/ip_network_primary_sample6
-wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/gfwlist/gfwlist.txt -nv -O files/overture/domain_alternative_sample
-wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/chinalist/chinalist.txt -nv -O files/overture/domain_primary_sample

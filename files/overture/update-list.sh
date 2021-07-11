222a13309968761056650ea2e4ad7b0895a4457c
cielpy <beijiu572@gmail.com>
2021-07-11 20:24:10 +0800

    use clang ip list, instead of pexcn daily

--- files/overture/update-list.sh
+++ files/overture/update-list.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 set -o pipefail
 
-wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/chnroute/chnroute.txt -nv -O ip_network_primary
-wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/chnroute/chnroute-v6.txt -nv -O ip_network_primary6
+wget https://ispip.clang.cn/all_cn.txt -nv -O ip_network_primary
+wget https://ispip.clang.cn/all_cn_ipv6.txt -nv -O ip_network_primary6
 cat ip_network_primary6 >> ip_network_primary
 rm ip_network_primary6
 wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/gfwlist/gfwlist.txt -nv -O domain_alternative

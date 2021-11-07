c43af97128f83b90bf7425d144a894e9c422d850
cielpy <beijiu572@gmail.com>
2021-11-07 17:47:27 +0800

    fix script broken on openwrt with wget

--- files/overture/overture-cron.sh
+++ files/overture/overture-cron.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
-cd /etc/overture
-./update-list.sh
-/etc/init.d/overture restart
+#!/bin/sh -e
+cd "$(dirname "$0")"
+set -o pipefail
+
+wget https://ispip.clang.cn/all_cn.txt -O ip_network_primary
+wget https://ispip.clang.cn/all_cn_ipv6.txt -O ip_network_primary6
+cat ip_network_primary6 >> ip_network_primary
+rm ip_network_primary6
+wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/gfwlist/gfwlist.txt -O domain_alternative
+wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/chinalist/chinalist.txt -O domain_primary
